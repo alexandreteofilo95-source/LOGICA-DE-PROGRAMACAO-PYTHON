c8c05ee73715ed1b8063e346ca56eb6d3000b86f
alexandreteofilo95-source <alexandreteofilo95@gmail.com>
2026-09-10 23:42:27 -0300

    Add files via upload

--- 25-atividade.py
+++ 25-atividade.py
@@ -2,29 +2,32 @@ import os
 os.system('cls')
 
 #ENTRADA 
-print('== aviso'
 
-' ==')
 valor = float(input('digite o valor da compra: '))
-pagamento = int(input('digite 1 para pagamento a vista e 2 para a prazo: '))
-parcelado_x2 = pagamento / 2
-parcelado_x3 = pagamento / 3
-parcelado_x4 = pagamento / 4
-parcelado_x4 = pagamento / 5
-parcelado_x5 = pagamento / 6
+pagamento = float(input('digite 1 para pagamento a vista e 2 para a prazo: '))
 prazo = pagamento
-a_vista = pagamento * 0.10
+a_vista = valor - 10
+
 
 #PROCESSAMENTO
+match (pagamento):
+     case 1:
+          print(f'\nO valor do produto: {valor}')
+          print('Forma de pagamento: à vista')
+          print(f'Valor com desconto: {a_vista}')
+          print(f'total pra pagar: {a_vista}')
+     case 2:
+          print(f'\nvalor do produto: {valor}')
+          print(f'Forma de pagamneto: à prazo')
+          print('quantdade de parcelas: 6')
+          print('Valor por parcela R$ 16,66')
+          print(f'total à prazo: {prazo}')
+     case _:
+          print('digite 1 ou 2')
 
-match pagamento:
-    case 1:
-        print(f'vai ficar: {a_vista}')
-    case 2:
-        print(f'vai ficar: {prazo}')
-    case _:
-        print('coloque o numero 1 ou 2')
-print(pagamento)
-print('== fim ==')
 
 
+
+
+
+ 
